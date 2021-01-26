e63f371544bcecacab1354dd6c8009c875c4e634
doinaj <doinajosan@yahoo.com>
2021-01-26 14:52:06 -0600

    changing the same print statement that I have already changed in my movie branch, from the master branch to get a conflict

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,7 +2,7 @@ public class HelloTest {
 
     //testing my Hello
     public static void main(String[] args) {
-        System.err.println("Trying to make a conflict");
+        System.err.println("Trying to make a conflict - Now, I'm doing some changes here from the master branch");
     }
 
 }
