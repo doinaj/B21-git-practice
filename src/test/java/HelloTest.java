090de9b50de6eaefe9c3e3b576ac6bdaf09183be
doinaj <doinajosan@yahoo.com>
2021-01-26 14:49:10 -0600

    first commit in the movie branch, changed HelloTest.java

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,7 +2,8 @@ public class HelloTest {
 
     //testing my Hello
     public static void main(String[] args) {
-        System.err.println("Trying to make a conflict");
+        System.err.println("Trying to make a conflict - changing it inside movie branch");
+
     }
 
 }
