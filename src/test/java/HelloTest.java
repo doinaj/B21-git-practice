71670d5cab699290099c0244542f70df02b28b00
doinaj <doinajosan@yahoo.com>
2021-01-26 14:49:55 -0600

    second commit in the movie branch, changed HelloTest.java, added serr

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,7 @@ public class HelloTest {
     //testing my Hello
     public static void main(String[] args) {
         System.err.println("Trying to make a conflict - changing it inside movie branch");
+        System.err.println("Hopefully it's gonna give me a conflict this time");
 
     }
 
