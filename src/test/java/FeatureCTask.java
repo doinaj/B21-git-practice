39f11479ebbfb4976b099471c7ae92fcf8033a79
doinaj <doinajosan@yahoo.com>
2021-01-26 13:41:36 -0600

    Fixed the error in the feature-c branch

--- src/test/java/FeatureCTask.java
+++ src/test/java/FeatureCTask.java
@@ -2,5 +2,6 @@ public class FeatureCTask {
     public static void main(String[] args) {
         System.out.println("Enter your name");
         System.err.println("Incorrect input");
+        System.out.println("Please re-enter your name");
     }
 }
