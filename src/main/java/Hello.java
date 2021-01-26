78c20b89d67271c49c85abac8430bcb0b43f1579
doinaj <doinajosan@yahoo.com>
2021-01-26 14:45:08 -0600

    on the phone better

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
         //movie on the phone 10 min
         //movie on the phone 20 min
 
-        //final hour, another 10 min before going back to living room
+        //final hour, another 10 min before going back to living room, i actually like it on the phone
 
         //ooo i like this movie
 
